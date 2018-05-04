903834b3adf9c767c542468835346c1c78151be9
Sota Nakajima <snakajim@ucsc.edu>
2018-05-04 16:38:24 -0700

    Fixed layout of question. No more cut off.

--- app/src/main/java/com/treeki/treekii/QoTD.java
+++ app/src/main/java/com/treeki/treekii/QoTD.java
@@ -13,6 +13,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+
 public class QoTD extends AppCompatActivity {
 
     //initialize
@@ -26,14 +31,22 @@ public class QoTD extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qotd);
         //get date
-        final String date = "0101";
 
         QoTD = findViewById(R.id.QoTD);
 //        answer = findViewById(R.id.answer);
 
         //get Database ref
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("Questions").child("1").child("1").addListenerForSingleValueEvent(
+//        Date date = null; // your date
+//        Calendar cal = Calendar.getInstance();
+//        cal.setTime(date);
+//        int month_ = cal.get(Calendar.MONTH);
+//        final String month = Integer.toString(month_);
+//        int day_ = cal.get(Calendar.DAY_OF_MONTH);
+//        final String day = Integer.toString(day_);
+        final String month = "2";
+        final String day = "5";
+        mDatabase.child("Questions").child(month).child(day).addListenerForSingleValueEvent(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
@@ -42,7 +55,7 @@ public class QoTD extends AppCompatActivity {
 
                         //error handling
                         if (question == null) {
-                            Log.e(TAG, "Question at"+date+" is unexpectedly null");
+                            Log.e(TAG, "Question at"+month+"/"+day+" is unexpectedly null");
                             Toast.makeText(getApplicationContext(),"can't fetch question",Toast.LENGTH_SHORT).show();
                         }
                         //if no err, change the question
