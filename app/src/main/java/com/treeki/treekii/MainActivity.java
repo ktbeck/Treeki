ef593b61159da7ac6f98bab001afe4988f879106
Tangni <wangtangni96@gmail.com>
2018-05-04 18:22:55 -0700

    login function done for now, will add to firebase later

--- app/src/main/java/com/treeki/treekii/MainActivity.java
+++ app/src/main/java/com/treeki/treekii/MainActivity.java
@@ -3,19 +3,19 @@ package com.treeki.treekii;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText name;
     private EditText password;
-    private TextView info;
     private Button signin;
     private Button signup;
-    private int counter = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +25,7 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText)findViewById(R.id.etPassword);
         signin = (Button)findViewById(R.id.btnSignin);
         signup = (Button)findViewById(R.id.btnSignup);
-        info = (TextView)findViewById(R.id.tvInfo);
 
-        info.setText("No of attempts remaining:5");
 //        calling the login page with the button
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,11 +42,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 
 //    check with login
     public void signinFunc(){
-        
+        String signinName = name.getText().toString().trim(); //trims the end like @gmail.com
+        String signinPassword = password.getText().toString();
+
+//        check if name box and password box is empty.
+        if(TextUtils.isEmpty(signinName)){
+            Toast.makeText(this,"Please enter a valid email address.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(signinPassword)){
+            Toast.makeText(this,"Please enter a valid password.",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 
 //    private void validate(String userName, String userPassword){
