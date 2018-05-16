5105458bfa7491c603995c0698d5deff93249d1f
Vienna Chan <vhchan@ucsc.edu>
2018-05-16 13:59:38 -0700

    Added a comment

--- app/src/main/java/com/treeki/treekii/QoTD.java
+++ app/src/main/java/com/treeki/treekii/QoTD.java
@@ -78,6 +78,7 @@ public class QoTD extends AppCompatActivity {
 
     }
 
+    //submits the QoTD
     public void submit(View view) {
         if (month.length() == 1) month = "0"+month;
         if (day.length() == 1) day = "0"+day;
