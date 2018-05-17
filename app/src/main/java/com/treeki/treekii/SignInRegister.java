a218ac425a714bac79d8cbe18e5d83590f2a4965
Sota Nakajima <snakajim@ucsc.edu>
2018-05-17 14:02:05 -0700

    Skips QoTD if already filled out. Would like to move code to a splash screen.

--- app/src/main/java/com/treeki/treekii/SignInRegister.java
+++ app/src/main/java/com/treeki/treekii/SignInRegister.java
@@ -17,15 +17,29 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
 
 public class SignInRegister extends AppCompatActivity{
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
+    private FirebaseUser user;
     private static final String TAG = "MainActivity";
 
     private EditText name;
     private EditText password;
     private Button signin;
     private Button signup;
+    String month;
+    String day;
+    String year;
+    String date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +49,22 @@ public class SignInRegister extends AppCompatActivity{
         password = (EditText)findViewById(R.id.etPassword);
         signin = (Button)findViewById(R.id.btnSignin);
         signup = (Button)findViewById(R.id.btnSignup);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        Calendar cal = Calendar.getInstance();
+        month = Integer.toString(cal.get(Calendar.MONTH)+1);
+        day = Integer.toString(cal.get(Calendar.DATE));
+        year = Integer.toString(cal.get(Calendar.YEAR));
+        if (month.length() == 1) month = "0"+month;
+        if (day.length() == 1) day = "0"+day;
+        date = month+"-"+day+"-"+year;
 
         //initializing firebase auth object
         firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
         if (user != null) {
-            Log.i(TAG,"Signed in: "+user.getUid());
-            startQoTD();
+            goToNextActivity();
         }
 
 
@@ -90,7 +113,7 @@ public class SignInRegister extends AppCompatActivity{
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
                                 Toast.makeText(SignInRegister.this,"Successfully logged in", Toast.LENGTH_LONG).show();
-                                startQoTD();
+                                goToNextActivity();
                                 finish();
                             } else {
                                 Log.e(TAG, "Login unsuccessful: " + task.getException().getMessage());
@@ -108,5 +131,66 @@ public class SignInRegister extends AppCompatActivity{
     private void startQoTD() {
         Intent intent = new Intent(SignInRegister.this,QoTD.class);
         startActivity(intent);
+    }
+    private void startJournal() {
+        Intent intent = new Intent(SignInRegister.this,Journal.class);
+        startActivity(intent);
+    }
+
+    private void goToNextActivity() {
+        Log.i(TAG,"Signed in: "+user.getUid());
+
+        mDatabase.child("Users").child(user.getUid()).child(date).child("QoTD").child("answer").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //get question at /Questions/0101
+                        String answer = dataSnapshot.getValue(String.class);
+
+                        //error handling
+                        if (answer == null) {
+                            Log.i(TAG,"User signed in but has not filled in QoTD.");
+                            startQoTD();
+                        }
+
+                        else {
+
+                            mDatabase.child("Users").child(user.getUid()).child(date).child("Journal").child("answer").addListenerForSingleValueEvent(
+                                    new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            //get question at /Questions/0101
+                                            String answer = dataSnapshot.getValue(String.class);
+
+                                            //error handling
+                                            if (answer == null) {
+                                                Log.i(TAG,"User signed in and finished QoTD but not journal");
+                                                startJournal();
+                                            }
+                                            //if no err, change the question
+                                            else {
+                                                //GOTO MAIN MENU
+                                                //TODO: MAIN MENU INTENT GOES HERE
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+                                            Log.w(TAG, "get Journal answer onCancelled", databaseError.toException());
+                                        }
+                                    }
+                            );
+
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "get QoTD answer onCancelled", databaseError.toException());
+                    }
+                }
+        );
+
     }
 }
