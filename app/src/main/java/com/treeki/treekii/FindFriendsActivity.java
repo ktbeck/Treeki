2d36cc0b26b76d546c5d73012118fc8841cb23bb
Tangni <wangtangni96@gmail.com>
2018-05-21 21:01:45 -0700

    find friends

---
+++ app/src/main/java/com/treeki/treekii/FindFriendsActivity.java
@@ -0,0 +1,13 @@
+package com.treeki.treekii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_friends);
+    }
+}
