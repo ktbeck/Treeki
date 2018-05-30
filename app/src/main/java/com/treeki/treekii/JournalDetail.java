e8ea149ffc2f8ec2d720f33977304a39555473bb
Sota Nakajima <snakajim@ucsc.edu>
2018-05-30 01:16:00 -0700

    edit/delete buttons only if journal is from today

--- app/src/main/java/com/treeki/treekii/JournalDetail.java
+++ app/src/main/java/com/treeki/treekii/JournalDetail.java
@@ -4,17 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class JournalDetail extends AppCompatActivity {
     private CheckBox priv;
     private String TAG = "JournalDetail";
+    String month;
+    String day;
+    String year;
+    String today;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_detail);
+
+        Calendar cal = Calendar.getInstance();
+        month = Integer.toString(cal.get(Calendar.MONTH)+1);
+        day = Integer.toString(cal.get(Calendar.DATE));
+        year = Integer.toString(cal.get(Calendar.YEAR));
+        if (month.length() == 1) month = "0"+month;
+        if (day.length() == 1) day = "0"+day;
+        today = month+"-"+day+"-"+year;
 
         Intent i = getIntent();
         String content = i.getStringExtra("content");
@@ -29,5 +45,12 @@ public class JournalDetail extends AppCompatActivity {
         priv = (CheckBox) findViewById(R.id.priv);
         priv.setChecked(checked);
         priv.setEnabled(false);
+
+        Button edit = (Button) findViewById(R.id.edit);
+        Button delete = (Button) findViewById(R.id.delete);
+        if (!date.equals(today)){
+            edit.setVisibility(View.INVISIBLE);
+            delete.setVisibility(View.INVISIBLE);
+        }
     }
 }
