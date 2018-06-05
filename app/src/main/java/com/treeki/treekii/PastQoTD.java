3a8058e25c8c8192868af1d777e18dfebba1898d
Sota Nakajima <snakajim@ucsc.edu>
2018-06-05 16:08:01 -0700

    Shows everything needed. also get values to send with intent.

--- app/src/main/java/com/treeki/treekii/PastQoTD.java
+++ app/src/main/java/com/treeki/treekii/PastQoTD.java
@@ -45,7 +45,7 @@ public class PastQoTD extends AppCompatActivity {
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(final DataSnapshot dataSnapshot) {
-                        for (DataSnapshot childSnapshot: dataSnapshot.getChildren()) { //for all children
+                        for (final DataSnapshot childSnapshot: dataSnapshot.getChildren()) { //for all children
                             date = childSnapshot.getKey();
                             Log.i(TAG,"key: "+date);
                             qotd = childSnapshot.child("QoTD").child("answer").getValue(String.class); //get qotd answer
@@ -61,16 +61,19 @@ public class PastQoTD extends AppCompatActivity {
                                 entries_.add(date+"\n"+qotd);
                                 ref.child("Questions").child(month_).child(day_).addValueEventListener(new ValueEventListener() {
                                     @Override
-                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                    public void onDataChange(DataSnapshot dataSnapshot2) {
                                         // This method is called once with the initial value and again
                                         // whenever data at this location is updated.
-                                        String question = dataSnapshot.getValue(String.class);
-                                        if (question.length() > 35)
-                                            question = question.substring(0, 35) + "...";
+                                        final String question = dataSnapshot2.getValue(String.class);
+                                        String trun_question = question;
+//                                        if (question.length() > 35)
+//                                            trun_question = question.substring(0, 35) + "...";
                                         Log.i(TAG, "Question: " + question);
 
                                         String[] parts = entries_.get(num).split("\n");
-                                        entries_.set(num,parts[0]+" "+question+"\n"+parts[1]);
+                                        Log.i(TAG,"PARTS[0]: "+parts[0]);
+                                        Log.i(TAG,"PARTS[1]: "+parts[1]);
+                                        entries_.set(num,parts[0]+" | "+trun_question+"\n"+parts[1]);
                                         Log.i(TAG, "entry: " + entries_.get(num));
                                         num += 1;
 
@@ -81,25 +84,33 @@ public class PastQoTD extends AppCompatActivity {
                                         }
                                         ArrayAdapter adapter = new ArrayAdapter(PastQoTD.this, android.R.layout.simple_list_item_1,entries); //set listview
                                         mListView.setAdapter(adapter);
-//                                        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //listview onclick handler
-//                                            @Override
-//                                            public void onItemClick(AdapterView<?> parent, View view, int position,
-//                                                                    long id) {
-//
-//                                                String date = ((TextView)view).getText().toString().split("\\n")[0];
-//                                                //Log.i(TAG,"date: "+date);
-//
-//                                                Intent QoTDDetail = new Intent(PastQoTD.this,QoTDDetail.class);
-//                                                String content = dataSnapshot.child(date).child("QoTD").child("answer").getValue(String.class);
-//                                                Boolean checked = dataSnapshot.child(date).child("QoTD").child("private").getValue(Boolean.class);
-//                                                Boolean faved = dataSnapshot.child(date).child("QoTD").child("favorite").getValue(Boolean.class);
+                                        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() { //listview onclick handler
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                                                    long id) {
+
+                                                String[] line = ((TextView)view).getText().toString().split("\n|\\| ");
+                                                String date = line[0];
+                                                String question = line[1];
+                                                String ans = line[2];
+                                                //Log.i(TAG,"date: "+date);
+
+                                                Intent QoTDDetail = new Intent(PastQoTD.this,QoTDDetail.class);
+                                                Boolean checked = childSnapshot.child("QoTD").child("private").getValue(Boolean.class);
+                                                Boolean faved = childSnapshot.child("QoTD").child("favorite").getValue(Boolean.class);
+                                                Log.i(TAG,"click: question: "+question);
+                                                Log.i(TAG,"click: date: "+date);
+                                                Log.i(TAG,"click: ans: "+ans);
+                                                Log.i(TAG,"click: checked: "+checked);
+                                                Log.i(TAG,"click: faved: "+faved);
+//                                                QoTDDetail.putExtra("question",question);
 //                                                QoTDDetail.putExtra("date",date);
-//                                                QoTDDetail.putExtra("content",content);
+//                                                QoTDDetail.putExtra("content",ans);
 //                                                QoTDDetail.putExtra("private",checked);
 //                                                QoTDDetail.putExtra("favorite",faved);
 //                                                startActivity(QoTDDetail);
-//                                            }
-//                                        });
+                                            }
+                                        });
                                     }
 
                                     @Override
