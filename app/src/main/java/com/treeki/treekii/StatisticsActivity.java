fc1a5cc42b1ce061970878a0c142bf0fa7615af4
Kyler <ktbeck@ucsc.edu>
2018-06-10 22:04:27 -0700

    finished statistics activity

--- app/src/main/java/com/treeki/treekii/StatisticsActivity.java
+++ app/src/main/java/com/treeki/treekii/StatisticsActivity.java
@@ -23,15 +23,20 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 
 public class StatisticsActivity extends AppCompatActivity {
     private static final String TAG = "Stats";
     private FirebaseUser user;
-    public Activity this_activity;
+    //    public Activity this_activity;
     String date;
     String mood;
     String month;
@@ -44,27 +49,29 @@ public class StatisticsActivity extends AppCompatActivity {
     private ArrayList<Date> low;
     int moodInt;
     SimpleDateFormat sdf = new SimpleDateFormat("M/d");
+    SimpleDateFormat daySDF = new SimpleDateFormat("d");
     String highString = "";
-    String lowString= "";
+    String lowString = "";
     long count = 2;
     long numChildren;
 
     final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
         Calendar cal = Calendar.getInstance();
-        month = Integer.toString(cal.get(Calendar.MONTH)+1);
+        month = Integer.toString(cal.get(Calendar.MONTH) + 1);
         day = Integer.toString(cal.get(Calendar.DATE));
         year = Integer.toString(cal.get(Calendar.YEAR));
-        date = month+"-"+day+"-"+year;
+        date = month + "-" + day + "-" + year;
         user = FirebaseAuth.getInstance().getCurrentUser();
 
 
     }
 
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
 //        TextView text = (TextView) findViewById(R.id.avg_mood);
 //        Log.i(TAG,"fish user: "+user.getUid());
@@ -76,65 +83,88 @@ public class StatisticsActivity extends AppCompatActivity {
                     @Override
                     public void onDataChange(final DataSnapshot dataSnapshot) {
                         long numChildren = dataSnapshot.getChildrenCount();
-                        for (DataSnapshot childSnapshot: dataSnapshot.getChildren()) { //if child /date
+                        for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) { //if child /date
 //                            Log.i(TAG, "COUNT " + count + " numChildren " +numChildren);
-                            low = new ArrayList<>();
+//                            low = new ArrayList<>();
                             ++count;
                             date = childSnapshot.getKey();
-                            Log.i(TAG,"key:" + date);
+//                            Log.i(TAG,"key:" + date);
                             mood = childSnapshot.child("mood").getValue(String.class); //get mood
-                                if(mood != null) {
-                                    moodInt = Integer.valueOf(mood);
-                                    String month_ = date.split("-")[0];
-                                    String day_ = date.split("-")[1];
-                                    if(month_.equals(month)) {
-                                        mood_.add(moodInt);
+                            if (mood != null) {
+                                moodInt = Integer.valueOf(mood);
+                                String month_ = date.split("-")[0];
+                                String day_ = date.split("-")[1];
+                                if (month_.equals(month)) {
+                                    mood_.add(moodInt);
 
-                                        Calendar date = Calendar.getInstance();
-                                        date.set(Calendar.YEAR, Integer.parseInt(year));
-                                        date.set(Calendar.MONTH, Integer.parseInt(month)-1);
-                                        date.set(Calendar.DAY_OF_MONTH, Integer.parseInt(day_));
-                                        Date utilDate = date.getTime();
-                                        dates_.add(utilDate);
-                                        Log.i(TAG, "COUNT " + count + " numChildren " +numChildren);
-                                        if(count >= numChildren) {
-                                            avg = averageArr(mood_);
-                                            high = bestDay(mood_, dates_);
-                                            low = worstDay(mood_, dates_);
-                                            int highCount = 0;
-                                            int lowCount = 0;
-                                            for (int i = 0; i < high.size(); i++) {
-                                                if(highCount > 0 && highCount != high.size()){
-                                                    highString += ", ";
-                                                }
-                                                highCount++;
-                                                String format = sdf.format(high.get(i));
-                                                highString += format;
+                                    Calendar date = Calendar.getInstance();
+                                    date.set(Calendar.YEAR, Integer.parseInt(year));
+                                    date.set(Calendar.MONTH, Integer.parseInt(month) - 1);
+                                    date.set(Calendar.DAY_OF_MONTH, Integer.parseInt(day_));
+                                    Date utilDate = date.getTime();
+                                    dates_.add(utilDate);
+
+//                                        Log.i(TAG,"FISHMAN \n\n");
+                                    for (int i = 0; i < dates_.size(); i++) {
+                                        String dayOfDates = daySDF.format(dates_.get(i));
+                                        dayOfDates.replaceFirst("^0*", "");
+//                                            Log.i(TAG,"FISHMAN day: "+dayOfDates);
+                                        if (dayOfDates.length() == 2) {
+                                            dates_.add(dates_.get(i));
+                                            dates_.remove(i);
+                                            mood_.add(mood_.get(i));
+                                            mood_.remove(i);
+                                        }
+                                    }
+
+                                    if (count >= numChildren) {
+                                        avg = averageArr(mood_);
+                                        high = bestDay(mood_, dates_);
+                                        low = worstDay(mood_, dates_);
+                                        int highCount = 0;
+                                        int lowCount = 0;
+                                        for (int i = 0; i < high.size(); i++) {
+                                            if (highCount > 0 && highCount != high.size()) {
+                                                highString += ", ";
                                             }
-                                            for (int i = 0; i < low.size(); i++) {
-                                                if(lowCount > 0 && lowCount != low.size()){
-                                                    lowString += ", ";
-                                                }
-                                                lowCount++;
-                                                String format = sdf.format(low.get(i));
-                                                lowString += format;
+                                            highCount++;
+                                            String format = sdf.format(high.get(i));
+                                            highString += format;
+                                        }
+                                        for (int i = 0; i < low.size(); i++) {
+                                            if (lowCount > 0 && lowCount != low.size()) {
+                                                lowString += ", ";
                                             }
+                                            lowCount++;
+                                            String format = sdf.format(low.get(i));
+                                            lowString += format;
                                         }
 
                                     }
-                                    GraphView graph = (GraphView) findViewById(R.id.graph);
-                                    TextView text = (TextView) findViewById(R.id.avg_mood);
-                                    TextView textHigh = (TextView) findViewById(R.id.high_mood);
-                                    TextView textLow = (TextView) findViewById(R.id.low_mood);
-                                    text.setText("Monthly Average is currently: "+ Double.toString(avg));
-                                    textHigh.setText("Your best days this month were: "+ highString);
-                                    textLow.setText("Your worst days this month were: "+ lowString);
+                                }
 
+//                                GraphView graph = (GraphView) findViewById(R.id.graph);
+                                TextView text = (TextView) findViewById(R.id.avg_mood);
+                                TextView textHigh = (TextView) findViewById(R.id.high_mood);
+                                TextView textLow = (TextView) findViewById(R.id.low_mood);
+                                DecimalFormat df = new DecimalFormat("#.##");
+                                String a = df.format(avg);
+                                text.setText("Monthly Average is currently: " + a);
+                                textHigh.setText("Your best days this month were: " + highString);
+                                textLow.setText("Your worst days this month were: " + lowString);
+                                if (count >= numChildren) {
+                                    GraphView graph = (GraphView) findViewById(R.id.graph);
                                     DataPoint[] dp = new DataPoint[mood_.size()];
-                                    for(int i=1; i < mood_.size() + 1; i++) {
-                                        if (mood_.get(i-1) != null) {
-                                            dp[i-1] = new DataPoint(dates_.get(i-1), mood_.get(i-1));
-                                            Log.i(TAG, "DP[i]" + dp[i-1]);
+                                    for (int i = 1; i < mood_.size() + 1; i++) {
+                                        if (mood_.get(i - 1) != null) {
+                                            String dayOfDates = daySDF.format(dates_.get(i - 1));
+                                            dayOfDates.replaceFirst("^0*", "");
+                                            int dayVal = Integer.parseInt(dayOfDates);
+                                            int currMood = mood_.get(i - 1);
+                                            Log.i(TAG, "currmood " + currMood);
+                                            DataPoint check = new DataPoint(dayVal, currMood);
+                                            dp[i - 1] = check;
+                                            Log.i(TAG, "values I should have" + dayVal + currMood);
                                         }
                                     }
                                     LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dp);
@@ -142,28 +172,31 @@ public class StatisticsActivity extends AppCompatActivity {
                                     graph.getViewport().setMaxX(Integer.parseInt(day));
                                     graph.getViewport().setMinY(0);
                                     graph.getViewport().setMaxY(10);
-                                    graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(StatisticsActivity.this));
 
                                     graph.getGridLabelRenderer().setNumHorizontalLabels(Integer.parseInt(day));
-                                    graph.getGridLabelRenderer().setNumVerticalLabels(11);
+                                    graph.getGridLabelRenderer().setNumVerticalLabels(6);
                                     graph.getViewport().setYAxisBoundsManual(true);
                                     graph.getViewport().setXAxisBoundsManual(true);
+                                    graph.getGridLabelRenderer().setHumanRounding(true);
                                     graph.addSeries(series);
                                 }
                             }
                         }
+                    }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-                        Log.w(TAG, "Error", databaseError.toException());
+//                        Log.w(TAG, "Error", databaseError.toException());
                     }
                 }
         );
-
     }
-    private double averageArr(ArrayList <Integer> mood) {
+
+
+
+    private double averageArr(ArrayList<Integer> mood) {
         Integer sum = 0;
-        if(!mood.isEmpty()) {
+        if (!mood.isEmpty()) {
             for (Integer i : mood) {
                 sum += i;
 
@@ -172,49 +205,58 @@ public class StatisticsActivity extends AppCompatActivity {
         }
         return sum;
     }
-    private ArrayList<Date> worstDay(ArrayList <Integer> mood, ArrayList <Date> dates) {
+
+    private ArrayList<Date> worstDay(ArrayList<Integer> mood, ArrayList<Date> dates) {
         Integer low = 11;
         ArrayList<Date> array = new ArrayList<>();
-        if(!mood.isEmpty()) {
-            for (int i = 0; i< mood.size(); i++) {
-                if(mood.get(i) < low) {
+        if (!mood.isEmpty()) {
+            for (int i = 0; i < mood.size(); i++) {
+                if (mood.get(i) < low) {
                     low = mood.get(i);
                 }
             }
-            for(int i = 0; i< mood.size(); i++){
-                if(mood.get(i) == (low)){
+            for (int i = 0; i < mood.size(); i++) {
+                if (mood.get(i) == (low)) {
                     array.add(dates.get(i));
                 }
             }
             return array;
-        }
-        else{
+        } else {
             return array;
         }
     }
-    private ArrayList<Date> bestDay(ArrayList <Integer> mood, ArrayList <Date> dates) {
+
+    private ArrayList<Date> bestDay(ArrayList<Integer> mood, ArrayList<Date> dates) {
         Integer high = 0;
         ArrayList<Date> array = new ArrayList<>();
-        if(!mood.isEmpty()) {
-            Log.i(TAG, "test print");
-            for (int i = 0; i< mood.size(); i++) {
-                if(mood.get(i) > high) {
+        if (!mood.isEmpty()) {
+//            Log.i(TAG, "test print");
+            for (int i = 0; i < mood.size(); i++) {
+                if (mood.get(i) > high) {
                     high = mood.get(i);
                 }
             }
-            for(int i = 0; i < mood.size(); i++){
-                if(mood.get(i) == (high)){
-                    Log.i(TAG, "high" + mood.get(i));
+            for (int i = 0; i < mood.size(); i++) {
+                if (mood.get(i) == (high)) {
+//                    Log.i(TAG, "high" + mood.get(i));
                     array.add(dates.get(i));
                 }
             }
-            for(int i = 0; i<array.size(); i++) {
-                Log.i(TAG, "print in high " + array.get(i));
+            for (int i = 0; i < array.size(); i++) {
+//                Log.i(TAG, "print in high " + array.get(i));
             }
             return array;
-        }
-        else{
+        } else {
             return array;
         }
+    }
+
+    public static boolean contains(ArrayList<Integer> arr, int dayVal, int moodVal) {
+        for (int k = 0; k < arr.size(); k += 2) {
+            if (arr.get(k) == dayVal && arr.get(k + 1) == moodVal) {
+                return true;
+            }
+        }
+        return false;
     }
 }
